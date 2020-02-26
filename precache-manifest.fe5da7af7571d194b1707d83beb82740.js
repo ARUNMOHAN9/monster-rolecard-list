12694020e73b98617712aa883427b3a8c279e12e
Arun Mohan <arunmh7@gmail.com>
2020-02-26 22:36:17 +0530

    Updates

---
+++ precache-manifest.fe5da7af7571d194b1707d83beb82740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f3e97394abb26fa8db969bc0a641f4",
+    "url": "/monster-rolecard-list/index.html"
+  },
+  {
+    "revision": "9ff9192c9673796f6767",
+    "url": "/monster-rolecard-list/static/css/main.9e9e57e2.chunk.css"
+  },
+  {
+    "revision": "e053c4650939392ab8c9",
+    "url": "/monster-rolecard-list/static/js/2.dc38e0e4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolecard-list/static/js/2.dc38e0e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ff9192c9673796f6767",
+    "url": "/monster-rolecard-list/static/js/main.048d6aa8.chunk.js"
+  },
+  {
+    "revision": "32859773e5321223f3e0",
+    "url": "/monster-rolecard-list/static/js/runtime-main.537e9a00.js"
+  }
+]);
